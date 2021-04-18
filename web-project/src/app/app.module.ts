62cc4b79586acb1f7493fba97e17625768e47365
tvha23 <ramazan.plt26@gmail.com>
2021-04-18 15:28:24 +0600

    commit

--- web-project/src/app/app.module.ts
+++ web-project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
